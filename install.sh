d101a8c45269755b878cba768e8213fd9969fdbf
luodanwg <luodan.wg@gmail.com>
2017-06-16 23:59:18 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -2,11 +2,9 @@ set -e
 set -x
 
 rm /opt/gopath/src/DNA -rf
-cd /opt/gopath/src;git clone https://github.com/luodanwg/DNA.git -b V0.6pre
+cd /opt/gopath/src;git clone https://github.com/luodanwg/DNA.git -b v0.6Pre
 cd /opt/gopath/src/DNA;glide install
-cp /opt/gopath/src/DNASample/main.go /opt/gopath/src/DNA -r
 go build -o /opt/gopath/src/DNASample/DNA main.go
+go build -o /opt/gopath/src/DNASample/nodectl nodectl.go
 cd /opt/gopath/src/DNASample
 rm /opt/gopath/src/DNASample/Chain -rf
-rm /opt/gopath/src/DNASample/config.json
-cp /opt/gopath/src/config/config.json /opt/gopath/src/DNASample/
